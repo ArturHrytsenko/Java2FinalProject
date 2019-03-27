497771bd1ab78fdcb3ee144afbb820ea7d55c78e
artur <artur@LAPTOP-1NI3SI6V>
2019-03-27 13:44:31 -0400

    CSV parser functional but needs organization, need income class

---
+++ src/draftoffinalassignment/DraftOfFinalAssignment.java
@@ -0,0 +1,19 @@
+
+
+package draftoffinalassignment;
+
+/**
+ *
+ * @author shann
+ */
+public class DraftOfFinalAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       javafx.application.Application.launch(CSVParser.class);
+       
+    }
+
+}
